9c1d5cd172a42e6c4c920efc58ebaaf774627204
sanaiylatariq <sanaiylatariq517@gmail.com>
2024-07-09 13:12:42 +0500

    Add files via upload

---
+++ sunny.js
@@ -0,0 +1,90 @@
+// // // scope level and mini hosting
+
+
+// // // jo chz fun1 m y usy kid yani funct two access karskta hay but two wli ko one access nh ktskta q k wo jld closed hojata hay
+// // function one(){
+// //     const name1 = "sanaiyla"
+// // function two(){
+// //     const website = "utube"
+// //     console.log(name1);
+// // }
+// // // console.log(website);
+// // two()
+
+// // }
+// // one()
+// // function three(){
+// //     const nam2 = "kato"
+// //     function four(){
+// //         console.log(nam2);
+// //     }
+// //     console.log(nam2);
+// // four()
+// // }
+// // three()
+// // function five(){
+// //     const hello = "bundo"
+// //     function six(){
+// //         console.log(hello);
+// //     }
+// //     six()
+// // }
+// // five()
+// // if(true){
+// //     const username = "sanaiyla"
+// //     if(username==="sanaiyla"){
+// //         const website = " youtube"
+// // console.log(username + website);    }
+// // }
+// // // console.log(website);
+// // // console.log(username);
+// function one(){
+//     const name1 = "sanaiyla"
+// function two(){
+//     const good = " is a good girl"
+//     // console.log(`${name1} ${good}`);
+//     console.log(name1 + good);
+// }
+// // two()
+
+// }
+// // one()
+// if(true){
+//     const username = "bundo"
+//       if(username=="bundo"){
+//   const username1 = " channel"
+// console.log(username+username1); }
+  
+//     }  
+//     if (true){
+// const username3 = "sohna buda"
+// if(username3=="sohna buda"){
+//     const abba = " hy mera abba"
+//     console.log(username3+abba);
+// }    
+// }
+
+
+// // //////////////////       interesting          ////////////////
+
+// console.log(addone(5));
+
+// function addone(num){
+//     return num + 1
+// }
+// console.log(addtwo(5));
+// // junhi fun ko var mn declare krdengy to usko initialize karna impossible hoga, yani log apo funct k bd dena hay not like upper case jahan ap log upar b dyskty thy yani pre declaration
+
+// const addtwo = function(num){
+//     return num + 2
+// }
+// console.log(addtwo(5));
+
+console.log(sunny(3));
+function sunny(num){
+    return num * 3
+}
+const addtwo = function(num){
+    return num / 9
+}
+console.log(addtwo(18));
